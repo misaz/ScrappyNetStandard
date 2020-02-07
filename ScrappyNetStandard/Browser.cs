e95f8db8640d03f9779eb4de148fded094845156
Michal Žůrek <zurmich@volny.cz>
2020-02-07 18:34:18 +0100

    Added .NET Standard version of Scrappy.

---
+++ ScrappyNetStandard/Browser.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CsQuery;
+
+namespace Scrappy
+{
+    public enum HttpVerb
+    {
+        Get,
+        Post
+    }
+
+    public class Browser
+    {
+        public HttpClient HttpClient { get; set; }
+
+        static Browser()
+        {
+            System.Net.ServicePointManager.Expect100Continue = false;
+        }
+
+        //=========================================================================================
+        /// <summary>
+        /// Creates a new Browser with default settings.
+        /// </summary>
+        //=========================================================================================
+        public Browser()
+        {
+            DocType = DocType.Default;
+            ParsingOptions = HtmlParsingOptions.Default;
+            ParsingMode = HtmlParsingMode.Auto;
+            HttpClient = new HttpClient();
+
+            //Client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml");
+            //Client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+            HttpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+            HttpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Charset", "ISO-8859-1");
+
+
+        }
+
+        //=========================================================================================
+        /// <summary>
+        /// Creates a new Browser with the injected HttpClient.
+        /// </summary>
+        /// <param name="client">The HttpClient the Browser is going to use.</param>
+        //=========================================================================================
+        public Browser(HttpClient client)
+        {
+            HttpClient = client;
+            DocType = DocType.Default;
+            ParsingOptions = HtmlParsingOptions.Default;
+            ParsingMode = HtmlParsingMode.Auto;
+        }
+
+        public HtmlParsingMode ParsingMode { get; set; }
+
+        public HtmlParsingOptions ParsingOptions { get; set; }
+
+        public DocType DocType { get; set; }
+
+        public bool AutoDownloadResources { get; set; }
+
+        public async Task<WebPage> Open(string url)
+        {
+            var uri = new Uri(url);
+            var response = await HttpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            var content = await response.GetContentAsString();
+            var page = new WebPage(this, content, uri)
+            {
+                Response = response
+            };
+
+            if (AutoDownloadResources)
+            {
+                await Task.WhenAll(page.Resources.Select(res => res.Response));
+            }
+
+            return page;
+        }
+
+
+
+        public async Task<WebPage> OpenWithFormData(string url, HttpVerb method, Dictionary<string, string> formData, bool asJson = false, string contentType = "application/x-www-form-urlencoded")
+        {
+            if (method == HttpVerb.Get)
+            {
+                var uri = new Uri(url);
+                var key = !string.IsNullOrWhiteSpace(uri.Query) ? '&' : '?';
+
+                var geturi = new Uri(uri, key + formData.ToQuery());
+
+                return await Open(geturi.ToString());
+            }
+
+
+            var httpcontent = new StringContent(asJson ? formData.ToJson() : formData.ToQuery(), 
+                Encoding.UTF8, asJson ? "application/json" : contentType);
+
+            return await PostWithFormData(url, httpcontent);
+        }
+
+        public async Task<WebPage> PostWithFormData(string url, HttpContent httpcontent)
+        {
+            var uri = new Uri(url);
+
+            var response = await HttpClient.PostAsync(uri, httpcontent);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+
+            var page = new WebPage(this, content, new Uri(uri.GetLeftPart(UriPartial.Path)))
+            {
+                Response = response
+            };
+
+            if (AutoDownloadResources)
+            {
+                await Task.WhenAll(page.Resources.Select(res => res.Response));
+            }
+
+
+
+            return page;
+        }
+    }
+}
---
+++ ScrappyNetStandard/WebResource.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsQuery.ExtensionMethods;
+using System.Net;
+using System.Net.Http;
+using ScrappyNetStandard;
+
+namespace Scrappy
+{
+    public class WebResource
+    {
+        public Uri Uri { get; private set; }
+        private readonly Browser browser;
+        private readonly WebPage webPage;
+        private readonly string nodeName;
+
+        private static Dictionary<string, string> MimeTypesByExtension { get; set; }
+
+        static WebResource()
+        {
+            MimeTypesByExtension = new Dictionary<string, string>();
+            Resources.MimeTypesByExtension.Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries).Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(x => x.Split('\t'))
+                .Where(x => x[0].Length > 0)
+                .ForEach(x => MimeTypesByExtension[x[0]] = x[1]);
+        }
+
+        internal WebResource(Uri uri, Browser browser, WebPage webPage, string nodeName)
+        {
+            this.Uri = uri;
+            this.browser = browser;
+            this.webPage = webPage;
+            this.nodeName = nodeName;
+
+            var path = uri.GetLeftPart(UriPartial.Path);
+            if (Path.HasExtension(path))
+            {
+                var extension = Path.GetExtension(path);
+                string guessMimeType;
+                if (MimeTypesByExtension.TryGetValue(extension, out guessMimeType))
+                {
+                    GuessMimeType = guessMimeType;
+                }
+            }
+
+        }
+
+        public string GuessMimeType { get; internal set; }
+
+        private Task<HttpResponseMessage> responseTask;
+
+        internal Task<HttpResponseMessage> Response
+        {
+            get
+            {
+                return responseTask ?? 
+                    (responseTask = browser.HttpClient.GetAsync(Uri)
+                                    .ContinueWith((response) => { GuessMimeType = response.Result.Content.Headers.ContentType.MediaType;
+                                    return response.Result;
+                                    }));
+            }
+        }
+
+        public async Task<byte[]> ReadAsBytes()
+        {
+            return await (await Response).GetContentAsBytes();
+        }
+
+        public async Task<string> ReadAsString()
+        {
+            return await (await Response).GetContentAsString();
+        }
+
+        public string NameSuggestion
+        {
+            get
+            {
+                if (Uri.PathAndQuery.Length > 5 && Uri.PathAndQuery.IndexOf('&') > 5)
+                {
+                    return Uri.GetLeftPart(UriPartial.Path).Substring(Uri.GetLeftPart(UriPartial.Path).LastIndexOf('/') + 1);
+                }
+                else if (Uri.OriginalString.Length < 5)
+                {
+                    return "index";
+                }
+                else
+                {
+                    return Uri.Host;
+                }
+            }
+        }
+
+    }
+}
